8d228880f6a052a618ca1159b7ea2372ddda55ed
KodiLeon <134138370+KodiLeon@users.noreply.github.com>
2023-05-20 15:10:26 +0300

    Add files via upload

---
+++ zadanie8.js
@@ -0,0 +1,14 @@
+let meOjct = new Map ([
+["den", 32],
+["olga", 26],
+["sveta", 13],
+["oleg", 45]
+	]);
+
+for (let name of meOjct.keys()) {
+  console.log(name);
+};
+
+for (let elem of meOjct) {
+	console.log(elem);
+}
